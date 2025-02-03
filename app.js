c64ca9eaca566231abc08f3cae18c5e2fa1e0f9e
LarryChen <larry110864gmail>
2025-02-04 01:20:06 +0800

    改變select顏色
    js防止enter、button送出表單，select根據ABCDEF+-變換顏色

--- app.js
+++ app.js
@@ -18,3 +18,59 @@
 // setTimeout(() => {
 //   animation.style.pointerEvents = "none";
 // }, 2500);
+
+function changeColor(target) {
+  if (target.value == "A" || target.value == "A-") {
+    target.style.backgroundColor = "lightgreen";
+    target.style.color = "black";
+  } else if (
+    target.value == "B" ||
+    target.value == "B-" ||
+    target.value == "B+"
+  ) {
+    target.style.backgroundColor = "yellow";
+    target.style.color = "black";
+  } else if (
+    target.value == "C" ||
+    target.value == "C-" ||
+    target.value == "C+"
+  ) {
+    target.style.backgroundColor = "orange";
+    target.style.color = "black";
+  } else if (
+    target.value == "D" ||
+    target.value == "D-" ||
+    target.value == "D+"
+  ) {
+    target.style.backgroundColor = "red";
+    target.style.color = "black";
+  } else if (target.value == "F") {
+    target.style.backgroundColor = "grey";
+    target.style.color = "white";
+  } else {
+    target.style.backgroundColor = "white";
+  }
+}
+
+//網頁防止enterkey送出表單
+window.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+  }
+});
+
+//button防止click送出表單
+let allbutton = document.querySelectorAll("button");
+allbutton.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+//select根據ABCD+-變換顏色
+let allSelects = document.querySelectorAll("select");
+allSelects.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    changeColor(e.target);
+  });
+});
