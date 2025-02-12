7063b020ed1afc3dbfca955e108e1e062d1a7b79
LarryChen <larry110864gmail>
2025-02-13 05:26:31 +0800

    排序程式碼
    排序程式碼前置，按鈕設置、演算法

--- app.js
+++ app.js
@@ -302,3 +302,88 @@ allTrash.forEach((trash) => {
     setGPA();
   });
 });
+
+//排序
+let btn1 = document.querySelector(".sort-descending");
+let btn2 = document.querySelector(".sort-ascending");
+btn1.addEventListener("click", () => {
+  handleSorting("descending");
+});
+btn2.addEventListener("click", () => {
+  handleSorting("ascending");
+});
+
+function handleSorting(direction) {
+  let graders = document.querySelectorAll("div-grader");
+  let objectArray = [];
+
+  for (let i = 0; i < graders.length; i++) {
+    let class_name = graders[i].children[0].value;
+    let class_number = graders[i].children[0].value;
+    let class_credit = graders[i].children[0].value;
+    let class_grade = graders[i].children[0].value;
+
+    if (
+      !(
+        class_name == "" &&
+        class_number == "" &&
+        class_credit == "" &&
+        class_grade == ""
+      )
+    ) {
+      let class_object = {
+        class_name,
+        class_number,
+        class_credit,
+        class_grade,
+      };
+    }
+    objectArray.push(class_object);
+  }
+
+  for (let i = 0; i < objectArray.length; i++) {
+    objectArray[i].class_grade_number = convertor(objectArray[i].class_grade);
+  }
+}
+
+function merge(a1, a2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < a1.length && j < a2.length) {
+    if (a1[i].class_grade_number > a2[j].class_grade_number) {
+      result.push(a2[j]);
+      j++;
+    } else {
+      result.push(a1[i]);
+      i++;
+    }
+  }
+
+  while (i < a1.length) {
+    result.push(a1[i]);
+    i++;
+  }
+  while (j < a2.length) {
+    result.push(a2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length == 0) {
+    return;
+  }
+
+  if (arr.length == 1) {
+    return arr;
+  } else {
+    let middle = Math.floor(arr.length / 2);
+    let left = arr.slice(0, middle);
+    let right = arr.slice(middle, arr.length);
+    return merge(mergeSort(left), mergeSort(right));
+  }
+}
